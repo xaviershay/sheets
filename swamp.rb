3ef22a1c65df5a6956628de313c9ddcebe3369f4
Xavier Shay <xavier@rhnh.net>
2009-02-20 18:12:48 +1100

    finish scoring in rb format

--- swamp.rb
+++ swamp.rb
@@ -21,15 +21,26 @@ end
 score = []
 
 r1 = Rhythm.new(*%w(8. 16 8 8))
-score << r1[%w(c d e c)] + %w(g'4 r4) + 
-r1[%w(c, d e c)] + %w(b4 r4) 
-notes = %w(
-  c8. d16 e8 c g'4 r4
-  c,8. d16 e8 c b4 r4
-  c8. d16 e8 c g'4 g16 f e d
-  c8 c16 d16 e8 c16 b d4 r4
-)
-#score << r1.apply(notes)
+score << [
+  r1[%w(c  d e c)], %w(g'4 r),
+  r1[%w(c, d e c)], %w(b4  r),
+  r1[%w(c  d e c)], %w(g'4 g16 f e d),
+  %w(c8 c16 d16 e8 c16 b d4 r4)
+].flatten
+
+score << [
+  r1[%w(c d e g)], %w(c4 r4),
+  r1[%w(e, f g e)], %w(a4 g16 f e d),
+  r1[%w(c d e g)], %w(c4 r4),
+  %w(c16 c c c c8 g b g b d)
+].flatten
+
+score << [
+  r1[%w(c d e c)], %w(a' a,16 g a g a8),
+  r1[%w(g a b g)], %w(e' e,16 d e d e8),
+  r1[%w(d e f d)], %w(b' a g f),
+  r1[%w(e f g b)], %w(e d d c)
+ ].flatten
 
 r2 = Rhythm.new(8, 16, 16)
 notes = %w(
@@ -47,21 +58,27 @@ class Lilypond
   def format(notes)
     elapsed = 0
     current_duration = '4'
-    notes.flatten.each do |note|
-      note_pitch    = note[/^[a-z',]+/, 0]
-      note_duration = (note[/\d+\.?$/, 0] || current_duration)
-      print "%-5s" % [[
-        note_pitch,
-        note_duration != current_duration ? note_duration : nil
-      ].compact.join]
-      current_duration = note_duration if note_duration
-      a = current_duration[/^\d+/, 0].to_i
-      b = current_duration[/\.$/, 0]
-      elapsed += 4 / a.to_f + (b ? 4 / (a * 2).to_f : 0)
-      if elapsed >= 4
-        puts 
-        elapsed = 0
+    print_block = lambda do |notes|
+      notes.flatten.each do |note|
+        note_pitch    = note[/^[a-z',]+/, 0]
+        note_duration = (note[/\d+\.?$/, 0] || current_duration)
+        print "%-5s" % [[
+          note_pitch,
+          note_duration != current_duration ? note_duration : nil
+        ].compact.join]
+        current_duration = note_duration if note_duration
+        a = current_duration[/^\d+/, 0].to_i
+        b = current_duration[/\.$/, 0]
+        elapsed += 4 / a.to_f + (b ? 4 / (a * 2).to_f : 0)
+        if elapsed >= 4
+          puts 
+          elapsed = 0
+        end
       end
+    end
+    notes.each do |block|
+      print_block[block]
+      puts
     end
     puts
   end
