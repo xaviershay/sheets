0f4ce06cd3caf156aca53b88fccfe5d50b6b7dd2
Xavier Shay <xavier@rhnh.net>
2009-02-20 19:57:31 +1100

    Extract lilypond.rb

--- swamp.rb
+++ swamp.rb
@@ -1,22 +1,5 @@
-class Rhythm
-  def initialize(*pattern)
-    @pattern = *pattern
-  end
-
-  def apply(notes)
-    notes = notes.dup
-    result = []
-    while !notes.empty?
-      run = notes.slice!(0, @pattern.length)
-      result << run.zip(@pattern).collect {|x| x.join }
-    end
-    result
-  end
-
-  def [](notes)
-    apply(notes)
-  end
-end
+require 'lilypond'
+include Lilypond
 
 score = []
 
@@ -50,37 +33,24 @@ notes = %w(
 )
 score << r2.apply(notes)
 
-class Lilypond
-  def initialize(options = {})
-    @options = options
-  end
+notes = %w(
+  r4 c'8. d16 e8 c g c e4 c8 e f4 d4
+  r4 c8. d16 e8 c g c e4 c8 e d4 b4
+  r4 c8. d16 e8 c e f g4 e8 g a4 f4
+  r4 e8. f16 g8 e g c c4 g8 c b4 g4
+  c
+)
+score << notes
+puts <<-EOS
+\\header {
+  title = Bubblegloop Swamp
+}
 
-  def format(notes)
-    elapsed = 0
-    current_duration = '4'
-    print_block = lambda do |notes|
-      notes.flatten.each do |note|
-        note_pitch    = note[/^[a-z',]+/, 0]
-        note_duration = (note[/\d+\.?$/, 0] || current_duration)
-        print "%-5s" % [[
-          note_pitch,
-          note_duration != current_duration ? note_duration : nil
-        ].compact.join]
-        current_duration = note_duration if note_duration
-        a = current_duration[/^\d+/, 0].to_i
-        b = current_duration[/\.$/, 0]
-        elapsed += 4 / a.to_f + (b ? 4 / (a * 2).to_f : 0)
-        if elapsed >= 4
-          puts 
-          elapsed = 0
-        end
-      end
-    end
-    notes.each do |block|
-      print_block[block]
-      puts
-    end
-    puts
-  end
-end
-Lilypond.new.format(score)
+\\relative {
+EOS
+Formatter.new.format(score)
+puts <<-EOS
+}
+
+\\version "2.12.2"
+EOS
